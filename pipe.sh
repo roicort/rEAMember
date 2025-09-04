523426bc685c988dd65deffdb50bb64e543ab0b2
zǝʇɹoɔ oɓıɹpoɹ <28023409+roicort@users.noreply.github.com>
2025-09-04 11:42:21 -0600

    Add timing for pipeline steps in pipe.sh
    Introduced a timeit function to measure and log the execution time of each pipeline step to times.txt. This helps track performance and duration of each command in the pipeline.

--- pipe.sh
+++ pipe.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
 CONFIG="./config/fashion.yml"
+TIMES_FILE="times.txt"
 
 set -e
 
-#uv run manage.py clean-logs
-#uv run manage.py autoencoder train --config $CONFIG
-#uv run manage.py get-embeddings --config $CONFIG
-#uv run manage.py autoencoder test --config $CONFIG
-#uv run manage.py classifier train --config $CONFIG
-#uv run manage.py classifier test --config $CONFIG
-#uv run manage.py get-bestparams --config $CONFIG
-uv run manage.py create-memories --config $CONFIG
+echo "" > $TIMES_FILE
+
+function timeit() {
+    CMD="$1"
+    LABEL="$2"
+    START=$(date +%s)
+    eval "$CMD"
+    END=$(date +%s)
+    ELAPSED=$((END - START))
+    echo "$LABEL: $ELAPSED seconds" >> $TIMES_FILE
+}
+
+timeit "uv run manage.py clean-logs" "clean-logs"
+timeit "uv run manage.py autoencoder train --config $CONFIG" "autoencoder train"
+timeit "uv run manage.py get-embeddings --config $CONFIG" "get-embeddings"
+timeit "uv run manage.py autoencoder test --config $CONFIG" "autoencoder test"
+timeit "uv run manage.py classifier train --config $CONFIG" "classifier train"
+timeit "uv run manage.py classifier test --config $CONFIG" "classifier test"
+timeit "uv run manage.py get-bestparams --config $CONFIG" "get-bestparams"
+timeit "uv run manage.py create-memories --config $CONFIG" "create-memories"
