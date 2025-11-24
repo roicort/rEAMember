1b0f88e58ac832485f7600758a324b8c5eb34722
zǝʇɹoɔ oɓıɹpoɹ <28023409+roicort@users.noreply.github.com>
2025-11-23 20:55:19 -0600

    Update pipeline to use best config and limit test size
    Switched to using the '.best.yml' config for plotting and memory creation steps. Added '--n 100' to encoder test and create-memories commands to limit the number of samples processed.

--- pipe.sh
+++ pipe.sh
@@ -20,9 +20,12 @@ function timeit() {
 timeit "uv run manage.py clean-logs" "clean-logs"
 timeit "uv run manage.py encoder train --config $CONFIG" "encoder train"
 timeit "uv run manage.py get-embeddings --config $CONFIG" "get-embeddings"
-timeit "uv run manage.py encoder test --config $CONFIG" "encoder test"
+timeit "uv run manage.py encoder test --config $CONFIG --n 100" "encoder test"
 timeit "uv run manage.py classifier train --config $CONFIG" "classifier train"
 timeit "uv run manage.py classifier test --config $CONFIG" "classifier test"
 timeit "uv run manage.py get-bestparams --config $CONFIG" "get-bestparams"
-timeit "uv run manage.py create-memories --config $CONFIG" "create-memories"
+
+CONFIG="${CONFIG/.yml/.best.yml}"
+
 timeit "uv run manage.py plot --config $CONFIG" "plot"
+timeit "uv run manage.py create-memories --config $CONFIG --n 100" "create-memories"
