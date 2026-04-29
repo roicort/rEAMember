300f6b91c0eaf74907ee9985a67a85eabec99371
zǝʇɹoɔ oɓıɹpoɹ <28023409+roicort@users.noreply.github.com>
2026-04-29 17:06:05 -0600

    Update text.pipe.sh

--- text.pipe.sh
+++ text.pipe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CONFIG="./config/dictionary-def.yml"
-TIMES_FILE="times.txt"
+TIMES_FILE="${CONFIG/.yml/.times.txt}"
 
 set -e
 
@@ -14,14 +14,21 @@ function timeit() {
     eval "$CMD"
     END=$(date +%s)
     ELAPSED=$((END - START))
-    echo "$LABEL: $ELAPSED seconds" >> $TIMES_FILE
+
+    DAYS=$((ELAPSED / 86400))
+    HOURS=$(((ELAPSED % 86400) / 3600))
+    MINUTES=$(((ELAPSED % 3600) / 60))
+    SECONDS=$((ELAPSED % 60))
+
+    printf "%s: %dd %02dh %02dm %02ds\n" "$LABEL" "$DAYS" "$HOURS" "$MINUTES" "$SECONDS" >> "$TIMES_FILE"
 }
 
 timeit "uv run manage.py get-embeddings --config $CONFIG" "get-embeddings"
 timeit "uv run manage.py encoder test --config $CONFIG --n 100" "encoder test"
-#timeit "uv run manage.py get-bestparams --config $CONFIG" "get-bestparams"
+timeit "uv run manage.py test-recall --config $CONFIG" "test-recall"
+timeit "uv run manage.py get-bestparams --config $CONFIG" "get-bestparams"
 
 CONFIG="${CONFIG/.yml/.best.yml}"
 
-#timeit "uv run manage.py plot --config $CONFIG" "plot"
+timeit "uv run manage.py plot --config $CONFIG" "plot"
 timeit "uv run manage.py create-memories --config $CONFIG --n 1000" "create-memories"
